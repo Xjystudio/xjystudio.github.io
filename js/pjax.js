3bf314d0abc914f2ed48c76683baf619f8040d93
谢佳宇 <xiejiayu55@outlook.com>
2025-10-25 22:52:26 +0800

    Site updated: 2025-10-25 22:52:23

---
+++ js/pjax.js
@@ -0,0 +1,1 @@
+!function(){let t;document.addEventListener("DOMContentLoaded",()=>{try{var a=window.Pjax||function(){};t=new a({selectors:["head title",".columns",".navbar-start",".navbar-end",".searchbox","#back-to-top","[data-pjax]",".pjax-reload"]})}catch(a){console.warn("PJAX error: "+a)}})}();
